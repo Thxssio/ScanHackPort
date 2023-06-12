f28dfeeff201b67916bebdc79479429281f9d3be
thxssio <thassiogomes44@gmail.com>
2023-06-12 13:09:27 -0300

    add prompt

--- README.md
+++ README.md
@@ -11,3 +11,16 @@
 `1024 até 49151` - São as chamadas “portas registradas”. Esse nome significa que as portas nesse intervalo podem ser registradas para protocolos específicos por grandes corporações.
 
 `49152 até 65535` - É o intervalo que engloba as portas dinâmicas e as portas privadas. Portas nesse intervalo podem ser usadas por praticamente qualquer um e, portanto, são as com maior probabilidade de haver vulnerabilidade.
+
+
+Instale os requerimentos
+
+```
+pip install -r requirements.txt
+```
+
+Executavel
+
+```
+python scanport.py
+```
---
+++ scanport.py
@@ -0,0 +1,44 @@
+import pyfiglet
+import sys
+import socket
+from datetime import datetime
+  
+ascii_banner = pyfiglet.figlet_format("ScanHackPort")
+print(ascii_banner)
+  
+target = input(str("Digite o ip do servidor: "))
+"""
+if len(sys.argv) == 2:
+    #target = socket.gethostbyname(sys.argv[1]) #target
+     target = input(str("Digite o ip do servidor: "))
+    
+else:
+    print("Quantidade inválida de argumento")
+ 
+"""
+
+# Add Banner
+print("-" * 50)
+print("Scaneando Target: " + target)
+print("Scaneando iniciado no:" + str(datetime.now()))
+print("-" * 50)
+  
+try:
+    
+    for port in range(1,65535):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+        result = s.connect_ex((target,port))
+        if result ==0:
+            print("Port {} is open | Porta {} está aberta".format(port))
+        s.close()
+         
+except KeyboardInterrupt:
+        print("\n Saindo do programa !!!!")
+        sys.exit()
+except socket.gaierror:
+        print("\n Não foi possivel neste Hostname !!!!")
+        sys.exit()
+except socket.error:
+        print("\ Servidor não responde !!!!")
+        sys.exit()
