d478b5363a0380dbab57127bd3c07ff8e3eb33eb
thxssio <thassiogomes44@gmail.com>
2023-06-15 21:20:10 -0300

    Update code | add bar

--- scanport.py
+++ scanport.py
@@ -2,11 +2,25 @@ import pyfiglet
 import sys
 import socket
 from datetime import datetime
-  
+import progressbar
+
 ascii_banner = pyfiglet.figlet_format("ScanHackPort")
 print(ascii_banner)
   
 target = input(str("Digite o ip do servidor: "))
+
+
+Wng = """ 
+
+0 até 1023 - São as consideradas “portas conhecidas”. Portas nesse intervalo são fornecidas apenas para serviços estabelecidos e grandes empresas ou centros de pesquisa de confiança.
+
+1024 até 49151 - São as chamadas “portas registradas”. Esse nome significa que as portas nesse intervalo podem ser registradas para protocolos específicos por grandes corporações.
+
+49152 até 65535 - É o intervalo que engloba as portas dinâmicas e as portas privadas. Portas nesse intervalo podem ser usadas por praticamente qualquer um e, portanto, são as com 
+maior probabilidade de haver vulnerabilidade.
+
+"""
+
 
 """
 if len(sys.argv) == 2:
@@ -16,21 +30,34 @@ else:
     print("Quantidade inválida de argumento")
  """
 
+i = 0
+bar = progressbar.ProgressBar(
+    widgets=[
+        progressbar.Percentage()
+    ])
+
 
 # Add Banner
 print("-" * 50)
-print("Scaneando Target: " + target)
+print("Scaneando Ip Andress: " + target)
 print("Scaneamento iniciado em:" + str(datetime.now()))
 print("-" * 50)
   
 try:
-    
-    for port in range(1,65535):
+    print(Wng)
+    port = int(input("Iniciar detecção a partir de qual porta? "))
+    LimitedPort = int(input("Finalizar detecção até qual porta? "))
+    while (port <= LimitedPort):
+        port = port + 1
+        i = i+1
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         socket.setdefaulttimeout(1)
-        result = s.connect_ex((target,port))
+        result = s.connect_ex((target, port))
+        bar.update(i)
         if result ==0:
-            print("Port {} is open".format(port))
+            print("Porta {} está aberta.".format(port))
+        else:
+            print("Porta {} está fechada.".format(port))    
         s.close()
          
 except KeyboardInterrupt:
